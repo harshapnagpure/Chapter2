e1b913f4d952f9e5cc580dac90db23a2cdf4d63c
Harsha Nagpure <harshanagpure04@gmail.com>
2026-01-22 13:52:46 +0530

    Add files via upload

---
+++ 13_summary.py
@@ -0,0 +1,115 @@
+##variable
+# a = 1 ##a is  variable work like a container
+# b = 2
+# name = "Harsha" #string
+# print(a+b)
+# print(name)
+
+# #Datatype:
+# a = 1#a is a integer
+# b = 5.33 # b is a float
+# c = "Harsha" # c is a string
+# d = False # d is a boolean
+# e = None # e is a none type varible
+
+
+## Rules for defining a variable:
+
+# @harsha = 56 # its not running
+#variable start from only alphabet and underscore. and does not contain any void space
+#can't start from the any digit
+#@does not contain any special character
+
+
+##Operators in python:
+'''
+1)Arithmetic Operator:
+addition: 2+3=5
+substraction:3-2=1
+multiplication:2*4=8
+division:4/2=2
+modulus:10%3=1
+floor division:10//3=3
+exponential/power:2**3=8
+
+#Example:
+a = 7
+b = 4
+c = a+b
+print(c)
+
+2)Assignment Operator:
+= : a=5
++=: x += 3.....x = x+3
+-=: x -= 2.....x = x-2
+*=: x *= 2.....x = x*2
+/=: x /= 2.....x = x/2
+
+Example:
+a = 4-2# Assign 4-2 in a
+print(a)
+b = 6
+b += 3 #Increment the value of b by 3 and then assign it to b
+b -= 3 #Decrement the value of b by 3 and then assign it to b
+print(b)
+
+3)Comparison Operators:
+==....equal to
+!=....not equal to
+>...greater than
+<...less than
+>=...greater than or equal to
+<=....less than or equal to
+
+Example:
+d = 5>4 #print true
+d = 5>5 #False
+d = 5!=7 #True
+d = 5==5 #True
+print(d)
+
+4)Logical Operators:
+and....both condition true
+or.....At least one condition is true
+not...reverse result
+
+Example:
+e = True or False    #Print True
+e = True and False   #print False
+e = not True   #print False
+print(not(True))        #print False
+print(e) 
+
+5)Bitwise Operators:
+&...AND
+~....NOT
+^...XOR
+`....`
+<<....left shift
+>>....right shift
+
+6)Membership Operators
+in....present int
+not in...not present
+
+7)Identity Operators:
+is....same object
+is not...Different object
+
+'''
+
+
+###Type 
+'''
+a = 31 #class <int>
+b = 31.2 #class <float>
+c = "Harsha" #class <string>
+t = type(a)  #class <int>
+print(t)
+
+'''
+
+
+
+
+
